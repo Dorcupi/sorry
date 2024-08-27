ed84c4ea688f59f33e3694038fb1bdf51fe034fe
dvyDvine <98676609+Dorcupi@users.noreply.github.com>
2024-08-27 00:40:32 +0000

    First commit

---
+++ scripts.js
@@ -0,0 +1,51 @@
+// scripts.js
+
+// Example apologies based on passwords
+const apologies = {
+    "1234": [
+        "making you worry about me",
+        "being late",
+        "forgetting your birthday",
+        "not being there for you",
+        "saying something hurtful"
+    ],
+    "5678": [
+        "not calling you back",
+        "canceling plans",
+        "not listening",
+        "being distant",
+        "acting selfish"
+    ]
+};
+
+let currentApologies = [];
+
+function submitPassword() {
+    const password = document.getElementById('password').value;
+    if (apologies[password]) {
+        currentApologies = apologies[password];
+        showSorryPage();
+    } else {
+        alert('Incorrect password');
+    }
+}
+
+function showSorryPage() {
+    document.getElementById('password-screen').style.display = 'none';
+    document.getElementById('sorry-page').style.display = 'block';
+    generateSorryGrid();
+}
+
+function generateSorryGrid() {
+    const grid = document.getElementById('sorry-grid');
+    grid.innerHTML = '';
+    for (let i = 0; i < 100; i++) {
+        const item = document.createElement('div');
+        item.className = 'sorry-item';
+        item.innerText = 'sorry';
+        item.onmouseover = function() {
+            document.getElementById('main-title').innerText = currentApologies[i % currentApologies.length];
+        };
+        grid.appendChild(item);
+    }
+}
